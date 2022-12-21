7b81f0eb21c8880ee3323f0dc674d6f6b737476d
Lucas Henrique <lucashenrique125@hotmail.com>
2022-12-20 21:06:43 -0300

    ajustes commits

--- Microservicos/GeekShopping.IdentityServer/Program.cs
+++ Microservicos/GeekShopping.IdentityServer/Program.cs
@@ -1,3 +1,5 @@
+using Duende.IdentityServer.AspNetIdentity;
+using Duende.IdentityServer.Services;
 using GeekShopping.IdentityServer.Configuration;
 using GeekShopping.IdentityServer.Initializer;
 using GeekShopping.IdentityServer.Model;
@@ -10,17 +12,14 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var connection = builder.Configuration["MySqlConnection:MySqlConnectionString"];
-builder.Services.AddDbContext<MySqlContext>(options => options.
-		UseMySql(
-			connection,
-			new MySqlServerVersion(new Version(8, 0, 31))
-		));
+
+var connection = builder.Configuration["MySQLConnection:MySQLConnectionString"];
+builder.Services.AddDbContext<MySqlContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 31))));
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
 	.AddEntityFrameworkStores<MySqlContext>()
 	.AddDefaultTokenProviders();
 
-var b = builder.Services.AddIdentityServer(options =>
+var build = builder.Services.AddIdentityServer(options =>
 {
 	options.Events.RaiseErrorEvents = true;
 	options.Events.RaiseInformationEvents = true;
@@ -28,34 +27,31 @@ var b = builder.Services.AddIdentityServer(options =>
 	options.Events.RaiseSuccessEvents = true;
 	options.EmitStaticAudienceClaim = true;
 }).AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
-  .AddInMemoryApiScopes(IdentityConfiguration.apiScopes)
-  .AddInMemoryClients(IdentityConfiguration.clients)
-  .AddAspNetIdentity<ApplicationUser>();
+.AddInMemoryApiScopes(IdentityConfiguration.apiScopes)
+.AddInMemoryClients(IdentityConfiguration.clients)
+.AddAspNetIdentity<ApplicationUser>();
 
 builder.Services.AddScoped<IDbInitializer, DbInitializer>();
-
-
-b.AddDeveloperSigningCredential();
+build.AddDeveloperSigningCredential();
 
 var app = builder.Build();
 
-IDbInitializer initializer = app.Services.CreateScope().ServiceProvider.GetRequiredService<IDbInitializer>();
+var scope = app.Services.CreateScope();
+var InitilizeDbServer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
 	app.UseExceptionHandler("/Home/Error");
 }
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 
 app.UseRouting();
-
 app.UseIdentityServer();
-
 app.UseAuthorization();
 
-initializer.Initializer();
+InitilizeDbServer.Initializer();
 
 app.MapControllerRoute(
 	name: "default",
